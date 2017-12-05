09cf44bf6f4963f4f31e2b8490f7547caab44dae
lk <laoki_someso@sina.com>
2017-12-05 16:38:50 +0800

    e3-manager

--- e3-manager/e3-manager-interface/src/main/java/com/e3/manager/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/com/e3/manager/service/ItemService.java
@@ -1,10 +1,13 @@
 package com.e3.manager.service;
 
 import com.e3.pojo.TbItem;
+import com.e3.utils.DatagridPagebean;
 
 public interface ItemService {
 	
 	
 	public TbItem findItemById(Long itemId );
+	
+	public DatagridPagebean findItemListByPage(Integer page,Integer rows);
 }
 	
--- e3-manager/e3-manager-service/src/main/java/com/e3/manager/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/com/e3/manager/service/impl/ItemServiceImpl.java
@@ -1,11 +1,17 @@
 package com.e3.manager.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.e3.manager.service.ItemService;
 import com.e3.mapper.TbItemMapper;
 import com.e3.pojo.TbItem;
+import com.e3.pojo.TbItemExample;
+import com.e3.utils.DatagridPagebean;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -17,6 +23,30 @@ public class ItemServiceImpl implements ItemService {
 		// TODO Auto-generated method stub
 		TbItem item = mapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+
+	@Override
+	public DatagridPagebean findItemListByPage(Integer page, Integer rows) {
+		// TODO Auto-generated method stub
+		//创建example对象
+		TbItemExample example = new TbItemExample();
+		//在查询之前设置分页
+		PageHelper.startPage(page, rows);
+		//执行查询
+		List<TbItem> list = mapper.selectByExample(example);
+		
+		//创建pageInfo对象  pageInfo分页插件封装了所有的分页信息
+		PageInfo<TbItem> pageInfo = new PageInfo<TbItem>(list);
+		
+		//创建DatafgridPagebean对象  封装分页信息
+		DatagridPagebean pagebean = new DatagridPagebean();
+		
+		pagebean.setTotal(pageInfo.getTotal());
+		pagebean.setRows(list);
+		
+		return pagebean;
+		
+		
 	}
 
 }
